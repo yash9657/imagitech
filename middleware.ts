17abcfe71bc08d189c5ee270e3d558ac19b8e3bb
Yash Bhalgat <yashbhalgat@Yashs-MacBook-Air.local>
2024-06-12 15:16:54 -0700

    tweaked the middleware.ts file

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
-import { auth, authMiddleware, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
- 
-export default authMiddleware({
-  publicRoutes: ['/api/webhooks/clerk']
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// add routes to be protected by auth here
+const isHomeRoute = createRouteMatcher(['/']);
+
+// By default clerk will not protect any routes. All routes must be explicitly protected.
+export default clerkMiddleware((auth, req) => {
+    if(isHomeRoute(req)) auth().protect();
 });
 
 export const config = {
